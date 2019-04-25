65dd52be48f9f10d58bf1186dcfb4c9577df763b
Raja1817 <rajag9328@gmail.com>
2019-04-25 12:09:07 +0530

    Updated

--- enteringHK.sh
+++ enteringHK.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sshpass -p "KrishnaSai#!*@" ssh -p 51920 corekrypc@192.168.1.139 
+sshpass -p "KrishnaSai#!*@" ssh -p 51920 corekrypc@192.168.1.139
+ 
+sshpass
---
+++ ip.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp "nancy@192.168.1.89:/home/desktop/trustscore.py"pi1@192.168.10.11:/home/pi
